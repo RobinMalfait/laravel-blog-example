525cb9d8b96101da919fe82d152c5947f77ab3a5
Robin Malfait <malfait.robin@gmail.com>
2021-03-12 14:46:54 +0100

    configure tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
